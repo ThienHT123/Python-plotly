18cb4c3967ca64ba51a6f236e07f673a140f883b
ThienHT <87663094+ThienHT123@users.noreply.github.com>
2022-06-29 13:59:24 +0700

    Scatter day

---
+++ Chart day.py
@@ -0,0 +1,13 @@
+import plotly.express as Chart
+import plotly.graph_objects as Graph
+import os
+
+os.system("color a")
+
+data_frame=Chart.data.tips()
+
+draw=Chart.scatter(data_frame,x='day',y='total_bill',title='My chart')
+
+print(draw)
+
+draw.show()
