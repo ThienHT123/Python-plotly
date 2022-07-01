ff65139b44bf437cb55d7b4049a73aa7c35014e9
ThienHT <87663094+ThienHT123@users.noreply.github.com>
2022-07-01 20:28:05 +0700

    Scatter day

---
+++ Chart day color sex.py
@@ -0,0 +1,13 @@
+import plotly.express as Chart
+import plotly.graph_objects as Graph
+import os
+
+os.system("color a")
+
+data_frame=Chart.data.tips()
+
+plot=Chart.scatter(data_frame,x='day',y='total_bill',color='sex',title='My chart')
+
+print(plot)
+
+plot.show()
